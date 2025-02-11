201a0fa15c39b44f5a9da524b78738085f823793
liam.conway234@gmail.com <liam.conway234@gmail.com>
2025-02-11 19:06:54 +0000

    Last one

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,19 +1,10 @@
-with
-    sales_margin as (select * from {{ ref("int_sales_margin") }}),
-
-    orders_grouped as (
-        select
-            orders_id,
-            date_date,
-            round(sum(revenue), 2) as revenue,
-            round(sum(quantity), 2) as quantity,
-            round(sum(purchase_cost), 2) as purchase_cost,
-            round(sum(margin), 2) as margin,
-        from sales_margin
-
-        group by 1, 2
-        order by 1 desc
-    )
-select *
-from
-    orders_grouped
+select
+   orders_id,
+   date_date,
+   Sum(revenue) as revenue,
+   sum(quantity) as quantity,
+   sum(purchase_price * quantity) as purchase_cost,
+   sum(revenue - (purchase_price * quantity)) as margin
+from {{ ref("int_sales_margin") }}
+group by date_date, orders_id
+order by orders_id
--- models/mart/mart_finance_days.sql
+++ models/mart/mart_finance_days.sql
@@ -1,22 +1,13 @@
-with 
-    orders_operational as ( select * from {{ ref("int_orders_operational") }}),
-
-    orders_oper_agg as (
-        select
-            date_date,
-            COUNT(orders_id) AS nb_transactions,
-            ROUND(SUM(revenue),0) AS revenue,
-            ROUND(AVG(revenue),1) AS average_basket,
-            ROUND(SUM(revenue)/COUNT(orders_id),1) AS average_basket_bis,
-            ROUND(SUM(margin),0) AS margin,
-            ROUND(SUM(operational_margin),0) AS operational_margin,
-            ROUND(SUM(purchase_cost),0) AS purchase_cost,
-            ROUND(SUM(shipping_fee),0) AS shipping_fee,
-            ROUND(SUM(logcost),0) AS logcost,
-            ROUND(SUM(ship_cost),0) AS ship_cost,
-            SUM(quantity) AS quantity
-        from orders_operational
-        group by  1
-        order by  1 desc
-    )
-select * from orders_oper_agg
+Select
+date_date as date,
+count(distinct orders_id) as nb_transactions,
+sum(revenue) as total_revenue,
+avg(revenue) as avg_basket,
+sum(operational_margin) as operational_margin,
+sum(purchase_cost) as total_purchase_cost,
+sum(shipping_fee) as total_shipping_cost,
+sum(logcost) as total_log_cost,
+sum(quantity) as total_quantity_of_products_sold
+from {{ref("int_orders_operational")}}
+group by date_date
+order by date_date desc
